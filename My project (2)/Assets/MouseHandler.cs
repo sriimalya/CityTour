657a59160b7f12adcdf4cf6fea877387be5802f3
Malya12 <malyasri0112@gmail.com>
2023-10-07 19:03:11 +0530

    Project Files

---
+++ My project (2)/Assets/MouseHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MouseHandler : MonoBehaviour
+{
+    public float horizontalSpeed = 1f;
+    public float verticalSpeed = 1f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+    }
+}
---
+++ My project (2)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+    public float MovementSpeed = 5;
+    public float gravity = 9.8f;
+    float yMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
+        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
+
+        characterController.Move(transform.forward * vertical + transform.right * horizontal);
+        if (characterController.isGrounded)
+        {
+            yMovement = 0;
+        }
+        else
+        {
+            yMovement -= gravity;
+            characterController.Move(new Vector3(0,yMovement, 0));
+        }
+    }
+}
